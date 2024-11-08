2cd8cf85bd05ebf7cbd04f0152c88efbb5590aa9
mauricio <mauriciosilvajunior12@gmail.com>
2024-11-08 09:42:03 -0300

    node-calc/usando modulos

---
+++ calc/app.js
@@ -0,0 +1,9 @@
+const somarFunc = require('./somar')
+const subFunc = require('./sub')
+const multiFunc = require('./multi')
+const divFunc = require('./div')
+
+console.log(somarFunc(3,5))
+console.log(subFunc(9,2))
+console.log(multiFunc(3,4))
+console.log(divFunc(9,2))
