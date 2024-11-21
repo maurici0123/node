e6561a75ce5da5f688241c600641420fd59d5bb4
mauricio <mauriciosilvajunior12@gmail.com>
2024-11-20 21:34:18 -0300

    node-sql/criando e alterando tabelas

--- sql/app.js
+++ sql/app.js
@@ -4,8 +4,24 @@ const sequelize = new Sequelize('sistemadecadastro', 'root', '2005', {
 	dialect: 'mysql'
 })
 
-sequelize.authenticate().then(function(){
+sequelize.authenticate().then(function () {
 	console.log('Conectado ao banco de dados')
-}).catch(function(erro){
+}).catch(function (erro) {
 	console.log(`Erro ao conectar ao banco de dados: ${erro}`)
+})
+
+const postagem = sequelize.define('postagem', {
+	titulo: {
+		type: Sequelize.STRING
+	},
+	descrição: {
+		type: Sequelize.TEXT
+	}
+})
+
+//postagem.sync({ force: true })
+
+postagem.create({
+	titulo: 'clean code',
+	descrição: 'livro de programação'
 })
