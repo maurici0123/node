4dc4b5062d1ab900cfad613853cae9e768f05b07
mauricio <mauriciosilvajunior12@gmail.com>
2024-11-22 11:00:23 -0300

    node-express/usando body-parser

--- handlebars/app.js
+++ handlebars/app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize')
 const app = express()
 
 // Template engine
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+// body parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 // connect database
 const sequelize = new Sequelize('sistemadecadastro', 'root', '2005', {
@@ -20,9 +25,14 @@ sequelize.authenticate().then(function () {
 
 //rotas
 app.get('/cad', function (req, res) {
-	res.send('testando')
+	res.render('formulario')
+})
+
+app.post('/add', function(req, res){
+	req.body.conteudo
+	res.send(`texto: ${req.body.titulo}, conteudo: ${req.body.conteudo}`)
 })
 
 app.listen(1221, function () {
-	console.log('servidor rodando na port:1221')
+	console.log('servidor rodando no http://localhost/:1221')
 })
