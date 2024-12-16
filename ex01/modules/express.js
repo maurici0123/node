ce6c99a741087afffc9cd2203ca83f8b13f7e538
mauricio <mauriciosilvajunior12@gmail.com>
2024-12-16 12:27:37 -0300

    node-ejs/integrandoo html

--- ex01/modules/express.js
+++ ex01/modules/express.js
@@ -6,6 +6,22 @@ const app = express()
 
 app.use(express.json())
 
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
+app.use((req, res, next) => {
+	console.log(`requeste type: ${req.method}`)
+	console.log(`date: ${new Date}`)
+
+	next()
+})
+
+app.get('/views/users', async (req, res) => {
+	const users = await UserModel.find({})
+
+	res.render('index', { users })
+})
+
 app.get('/users', async (req, res) => {
 	try {
 		const users = await UserModel.find({})
@@ -41,6 +57,17 @@ app.patch('/users/:id', async (req, res) => {
 		const id = req.params.id
 
 		const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true })
+		res.status(200).json(user)
+	} catch (error) {
+		res.status(500).send(error.message)
+	}
+})
+
+app.delete('/users/:id', async (req, res) => {
+	try {
+		const id = req.params.id
+
+		const user = await UserModel.findOneAndDelete(id)
 		res.status(200).json(user)
 	} catch (error) {
 		res.status(500).send(error.message)
