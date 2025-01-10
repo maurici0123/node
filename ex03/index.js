56f72289314fa232abec66da65b6a5bc306f333a
mauricio <mauriciosilvajunior12@gmail.com>
2025-01-10 17:54:48 -0300

    mexendo com as libs path e fs

---
+++ ex03/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
+const raiz = './arquivos'
+var data = []
+var aux = 0
+
+function listarArquivos(raiz) {
+	const fileOrPast = fs.readdirSync(raiz)
+
+	fileOrPast.map(arquivo => {
+		const caminho = path.join(raiz, arquivo)
+		
+		const stats = fs.statSync(caminho)
+		if (stats.isDirectory()) {
+			listarArquivos(caminho)
+		} else {
+			data[aux] = {
+				nome: path.basename(caminho),
+				caminho: caminho,
+				tamanho: `${stats.size} Bytes`,
+			}
+			aux++
+		}
+	})
+}
+
+listarArquivos(raiz)
+console.log(data)
